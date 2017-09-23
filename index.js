4182de904963dc70de0d0faf36155a116e3962bf
Ian Webster <ianw_git@ianww.com>
2017-09-23 12:04:52 -0700

    add polling support

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const http = require('http');
 
 const REGEX = /,([\d\.]+),/;
 
+let mostRecentValue = null;
+let mostRecentTime = null;
+let pollTimeout = null;
+
 function fetch(cb) {
   http.get('http://download.finance.yahoo.com/d/quotes.csv?s=^gspc&f=sl1p2', (resp) => {
     let body = '';
@@ -11,18 +15,46 @@ function fetch(cb) {
     resp.on('end', () => {
       const matches = REGEX.exec(body);
       if (!matches || matches.length < 2) {
-        cb(new Error('Could not find value in response.'), null);
+        cb && cb(new Error('Could not find value in response.'), null);
         return;
       }
       const result = parseFloat(matches[1]);
-      cb(null, result);
+      mostRecentValue = result;
+      mostRecentTime = new Date();
+      cb && cb(null, result);
     });
     resp.on('error', (e) => {
-      cb(e, null);
+      cb && cb(e, null);
     });
   });
 }
 
+function startPolling(interval = 1000 * 60 * 5) {
+  const fn = () => {
+    fetch((err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  };
+  fn();
+  pollTimeout = setInterval(fn, interval);
+}
+
+function stopPolling() {
+  clearInterval(pollTimeout);
+}
+
+function getMostRecent() {
+  return {
+    value: mostRecentValue,
+    timestamp: mostRecentTime,
+  }
+}
+
 module.exports = {
   fetch,
+  startPolling,
+  stopPolling,
+  getMostRecent,
 };
